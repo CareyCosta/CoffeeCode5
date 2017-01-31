56ba96ef17dc6f8a55fdf998734eb4b4993f822c
Carey Costa <careyxcosta@gmail.com>
2017-01-31 12:14:23 -0500

    exercises 1 and 2

--- cc.rb
+++ cc.rb
@@ -1,3 +1,4 @@
+#1
 numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a']
 suits= ['spades', 'hearts', 'diamonds', 'clubs']
 
@@ -6,7 +7,19 @@ deck = []
 numbers.each do |n|
   suits.each do |s|
     deck << [n, s]
-  end
+   end
+ end
+
+print deck
+
+#2
+
+arr = ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+
+def phone_number(a)
+  s = ""
+  s << "(" + [a[0], a[1], a[2]].join + ")" + " " + [a[3], a[4], a[5]].join + "-" + [a[6], a[7], a[8], a[9]].join
+    puts s
 end
 
-print deck 
+phone_number(arr)
